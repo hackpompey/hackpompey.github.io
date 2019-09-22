34fa5f9c28beb7b2f9e02498cff1ba9913b4e800
David Ralph <David.M.L.Ralph@gmail.com>
2019-09-22 11:58:58 +0100

    Header logo
    Added logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,48 +1,41 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import style from "./header.module.css"
 
 /**
  * The site header with navigation links
  */
-const Header = ({ siteTitle }) => {
-  const { site } = useStaticQuery(
+const Header = () => {
+  const data = useStaticQuery(
     graphql`
       query {
-        site {
-          siteMetadata {
-            title
+        logo: file(relativePath: { eq: "hack-pompey-logo-white.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 100) {
+              ...GatsbyImageSharpFluid_noBase64
+            }
           }
         }
       }
     `
   )
-
-  siteTitle = siteTitle || site.siteMetadata.title
+  console.log(data)
 
   return (
     <header className={style.header}>
       <nav>
         <Link to="/soon">Next Event</Link>
         <Link to="/media">Media</Link>
-        <h1>
-          <Link to="/">{siteTitle}</Link>
-        </h1>
+        <Link to="/" className={style.logo}>
+          <Img fluid={data.logo.childImageSharp.fluid} />
+        </Link>
         <Link to="/events">Past Events</Link>
         <Link to="/team">Team</Link>
       </nav>
     </header>
   )
-}
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
 }
 
 export default Header
