2756c970778b76efc2fcdc6b9cc0ccb922b4e563
David Ralph <David.M.L.Ralph@gmail.com>
2019-09-22 01:12:14 +0100

    Optimised background image
    - Background image is now served locally using gatsby-image-background
    
    - Added favicon
    
    - Removed clutter from starter

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,9 @@ import style from "./layout.module.css"
 import Header from "./header"
 import Footer from "./footer"
 
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+
 // Make FontAwesome icons available without specific imports
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
@@ -13,14 +16,31 @@ library.add(fab, fas)
  * The standard structure used in all pages
  */
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      background: file(relativePath: { eq: "images/background.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
   return (
     <div className={style.layout}>
       <Header />
-      <div className={style.content}>
-        {/* Display inner HTML elements */}
-        {children}
-      </div>
-      <Footer />
+      <BackgroundImage
+        Tag="section"
+        className={style.background}
+        fluid={data.background.childImageSharp.fluid}
+      >
+        <div className={style.content}>
+          {/* Display inner HTML elements */}
+          {children}
+        </div>
+        <Footer />
+      </BackgroundImage>
     </div>
   )
 }
