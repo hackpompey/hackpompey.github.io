ebc0c96f2d73945f59f06bc6d0111df3372f470b
David Ralph <David.M.L.Ralph@gmail.com>
2022-03-14 18:09:36 +0000

    update tagline

--- src/components/splash.js
+++ src/components/splash.js
@@ -51,7 +51,7 @@ const Splash = ({ currentEvent }) => {
       {/* If there's no upcoming event, use generic tagline */}
       {!currentEvent && (
         <div>
-          <h1>Hack Pompey | A Portsmouth based social hack event!</h1>
+          <h1>Hack Pompey | Meet New People, Make Something Awesome!</h1>
           <h3>
             For announcements and future events{" "}
             <a href="http://eepurl.com/glFL6H" target="_blank" rel="noopener noreferrer">Join our mailing list</a>
