2e8568eddedc03f76c4ab811b1e8ef47591224ad
David Ralph <David.M.L.Ralph@gmail.com>
2023-03-05 14:50:32 +0000

    banner automation

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,48 +1,67 @@
+import { useStaticQuery, graphql } from "gatsby"
 import * as style from "./banner.module.css"
 import React from "react"
 
 const Banner = () => {
-    const [marginTop, setMarginTop] = React.useState("6rem")
+  const [marginTop, setMarginTop] = React.useState("6rem")
 
-    const getWindowHeight = () => {
-        const distanceY = window.pageYOffset || document.documentElement.scrollTop
-        const shrinkOn = 200
-        setMarginTop(distanceY > shrinkOn ? "3.2rem" : "6rem")
+  const getWindowHeight = () => {
+    const distanceY = window.pageYOffset || document.documentElement.scrollTop
+    const shrinkOn = 200
+    setMarginTop(distanceY > shrinkOn ? "3.2rem" : "6rem")
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", getWindowHeight)
+    return function cleanup() {
+      window.removeEventListener("scroll", getWindowHeight)
     }
+  }, [])
 
-    React.useEffect(() => {
-        window.addEventListener("scroll", getWindowHeight)
-        return function cleanup() {
-        window.removeEventListener("scroll", getWindowHeight)
+  const data = useStaticQuery(graphql`
+    {
+      currentEvent: markdownRemark(
+        frontmatter: { tags: { in: "Current Event" } }
+      ) {
+        fields {
+          slug
         }
-    },[])
+        fileAbsolutePath
+        frontmatter {
+          dateText
+          location
+          registration_link
+        }
+      }
+    }
+  `)
 
-    return (
-        <div
-            className={style.banner}
-            style={{ top: marginTop }}
-        >
-            <div className={style.bannerInner}>
-                <div className={style.textContainer}>
-                    <span className={style.text}>
-                        Sat 25th March, 9am-5pm&nbsp;
-                    </span>
-                    <span className={style.text}>
-                        @ University of Portsmouth
-                    </span>
-                </div>
-                <div>
-                    <a
-                        className={style.bookButton}
-                        href="https://hackerfest.co.uk/e/4812fe6b-1dc7-4ec4-9e53-feb54a0f6ded"
-                        target="_blank" rel="noopener noreferrer"
-                    >
-                        Book your FREE ticket
-                    </a>
-                </div>
-            </div>
+  if(!data.currentEvent) return <div></div>
+
+  return (
+    <div className={style.banner} style={{ top: marginTop }}>
+      <div className={style.bannerInner}>
+        <div className={style.textContainer}>
+          <span className={style.text}>
+            {data.currentEvent.frontmatter.dateText}&nbsp;
+          </span>
+          <span className={style.text}>
+            @ {data.currentEvent.frontmatter.location}
+          </span>
         </div>
-    )
+        <div>
+          <a
+            className={style.bookButton}
+            href={data.currentEvent.frontmatter.registration_link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Book your FREE ticket
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Banner
